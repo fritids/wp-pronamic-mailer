26869713a4fb8d0c7592e38381a450d852faba9f
Leon Rowland <leon@rowland.nl>
2013-05-06 15:47:50 +0200

    Moved mail templates into folder. Got example mail template from mailchimp/Email-Blueprints

---
+++ templates/mail_shared/email-footer.php
@@ -0,0 +1,59 @@
+ <!-- // End Template Body \\ -->
+                                </td>
+                            </tr>
+                        	
+<tr>
+                            	<td align="center" valign="top">
+                                    <!-- // Begin Template Footer \\ -->
+                                	<table border="0" cellpadding="10" cellspacing="0" width="600" id="templateFooter">
+                                    	<tr>
+                                        	<td valign="top" class="footerContent">
+                                            
+                                                <!-- // Begin Module: Standard Footer \\ -->
+                                                <table border="0" cellpadding="10" cellspacing="0" width="100%">
+                                                    <tr>
+                                                        <td colspan="2" valign="middle" id="social">
+                                                            <div>
+                                                                &nbsp;<a href="">follow on Twitter</a> | <a href="">friend on Facebook</a> | <a href="">forward to a friend</a>&nbsp;
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td valign="top" width="350">
+                                                            <div mc:edit="std_footer">
+																<em>Copyright &copy; All rights reserved.</em>
+																<br />
+																
+																<strong>Our mailing address is:</strong>
+																<br />
+                                                            </div>
+                                                        </td>
+                                                        <td valign="top" width="190" id="monkeyRewards">
+                                                            <div>
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td colspan="2" valign="middle" id="utility">
+                                                            <div mc:edit="std_utility">
+                                                                &nbsp;<a href="">unsubscribe from this list</a> | <a href="">update subscription preferences</a>&nbsp;
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                                <!-- // End Module: Standard Footer \\ -->
+                                            
+                                            </td>
+                                        </tr>
+                                    </table>
+                                    <!-- // End Template Footer \\ -->
+                                </td>
+                            </tr>
+                        </table>
+                        <br />
+                    </td>
+                </tr>
+            </table>
+        </center>
+    </body>
+</html>
---
+++ templates/mail_templates/ftp_mail.php
@@ -0,0 +1,22 @@
+<?php include( '../mail_shared/email-header.php' ); ?>
+<table border="0" cellpadding="0" cellspacing="0" width="600" id="templateBody">
+    <tr>
+        <td valign="top" class="bodyContent">
+
+            <!-- // Begin Module: Standard Content \\ -->
+            <table border="0" cellpadding="20" cellspacing="0" width="100%">
+                <tr>
+                    <td valign="top">
+                        <div>
+                            <p>Test</p>
+                            <p>Testing 2</p>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+            <!-- // End Module: Standard Content \\ -->
+
+        </td>
+    </tr>
+</table>
+<?php include( '../mail_shared/email-footer.php' ); ?>
